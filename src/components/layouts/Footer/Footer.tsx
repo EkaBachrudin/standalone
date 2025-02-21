1d675d8f013f01e44a5c8d9667cc18098e1fae1e
Eka Bachrudin <eka.nursa@salt.co.id>
2025-02-21 18:22:07 +0700

    Adjutment footer

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,36 +1,50 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import { footerStaticData, socialStaticContent } from './Footer.data';
 import './Footer.scss';
 import type { FooterMenu, SocialContent } from './Footer.type';
 import Image from 'next/image';
 
 const FooterComponent = () => {
-  const footerData: FooterMenu[] = footerStaticData;
+  const [footerData, setfooterData] = useState(footerStaticData);
   const socialData: SocialContent = socialStaticContent;
 
-  const openChildMenu = (index: number) => {
-    footerData.map((item, i) =>
-        i === index ? { ...item, isOpen: !item.isOpen } : item
-      )
+  useEffect(() => {
+    setfooterData(footerData);
+  }, [footerData]);
+
+  const openMobileDropdown = (index: number) => {
+    setfooterData(prevMenuItems =>
+        prevMenuItems.map((item, i) =>
+            i === index ? { ...item, isOpen: !item.isOpen } : item
+        )
+    );
+
+      console.log(footerData)
   };
 
   return (
       <>
-        <div className='footer-wrapper'>
+        <div className='footer-wrapper pb-36'>
             <section className='section-app'>
-                <div className="title">Gain more with MyTelkomsel App</div>
-                <Image src="assets/images/tsel-app.svg" width={56} height={56} alt="tselapp" className='mt-4' />
+                <div>
+                    <div className="title">Gain more with MyTelkomsel App</div>
+                    <div className='sm:flex sm:gap-12 sm:items-start'>
+                        <Image src="assets/images/tsel-app.svg" width={56} height={56} alt="tselapp" className='mt-4' />
+                        <div>
+                            <div className='flex gap-2 mt-4'>
+                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                            </div>
 
-                <div className='flex gap-2 mt-4'>
-                    <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                    <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                            <div className='flex gap-2 mt-4'>
+                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className='flex gap-2 mt-4'>
-                    <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                    <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                </div>
-
-                
             </section>
 
             <hr className="border-t border-primary-rzaGrey40 my-4 mt-6" />
@@ -47,17 +61,19 @@ const FooterComponent = () => {
             <section className='section-menu'>
                 {footerData.map((menu, index) => (
                     <div key={index}>
-                        <button className='title' onClick={() => openChildMenu(index)}>
-                            <span>{menu.title}</span>
+                        <div className='title' onClick={() => openMobileDropdown(index)}>
+                            <div>{menu.title}</div>
                             <Image width={30} height={30} src={'/assets/icons/chevron-' + (menu.isOpen ? 'up.svg' : 'down.svg')} alt="down"/>
-                        </button>
-                        <ul>
-                            {menu.items.map((item, itemIndex) => (
-                                <li key={itemIndex}>
-                                    <a href={item.url}>{item.name}</a>
-                                </li>
-                            ))}
-                        </ul>
+                        </div>
+                        {menu.isOpen && (
+                            <ul>
+                                {menu.items.map((item, itemIndex) => (
+                                    <li key={itemIndex}>
+                                        <a href={item.url}>{item.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
                 ))}
             </section>
