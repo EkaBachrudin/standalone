a80b1c185cdd80b22c94a616a5c3f75c85e2654f
Eka Bachrudin <eka.nursa@salt.co.id>
2025-02-24 17:41:47 +0700

    footer

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -3,15 +3,30 @@
 import { useEffect, useState } from 'react';
 import { footerStaticData, socialStaticContent } from './Footer.data';
 import './Footer.scss';
-import type { FooterMenu, SocialContent } from './Footer.type';
+import { FooterMenu, SocialContent } from './Footer.type';
 import Image from 'next/image';
 
 const FooterComponent = () => {
   const [footerData, setfooterData] = useState(footerStaticData);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const socialData: SocialContent = socialStaticContent;
 
   useEffect(() => {
     setfooterData(footerData);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+  
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+        window.removeEventListener('resize', handleResize);
+
+        setfooterData(footerData);
+    };
+
   }, [footerData]);
 
   const openMobileDropdown = (index: number) => {
@@ -20,60 +35,58 @@ const FooterComponent = () => {
             i === index ? { ...item, isOpen: !item.isOpen } : item
         )
     );
-
-      console.log(footerData)
   };
 
   return (
       <>
         <div className='footer-wrapper pb-36'>
-            <section className='section-app'>
-                <div>
-                    <div className="title">Gain more with MyTelkomsel App</div>
-                    <div className='sm:flex sm:gap-12 sm:items-start'>
-                        <Image src="assets/images/tsel-app.svg" width={56} height={56} alt="tselapp" className='mt-4' />
-                        <div>
-                            <div className='flex gap-2 mt-4'>
-                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                            </div>
+              <div className='w-full xl:w-[60%]'>
+                  <section className='section-app'>
+                      <div>
+                          <div className="title">Gain more with MyTelkomsel App</div>
+                          <div className='lg:flex lg:gap-12 lg:items-start'>
+                              <Image src="assets/images/tsel-app.svg" width={56} height={56} alt="tselapp" className='mt-4' />
+                              <div>
+                                  <div className='flex gap-2 mt-4'>
+                                      <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                      <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                  </div>
 
-                            <div className='flex gap-2 mt-4'>
-                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                                <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                                  <div className='flex gap-2 mt-4'>
+                                      <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                      <Image src="assets/images/download-playstore.svg" width={144} height={40} alt="Google Play" />
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </section>
 
-            <hr className="border-t border-primary-rzaGrey40 my-4 mt-6" />
+                  <hr className="border-t border-primary-rzaGrey40 my-4 mt-6" />
 
-            <section className='section-social'>
-                <div className="title">{socialData.title}</div>
-                <div className='items'>
-                {socialData.items.map((item, index) => (
-                    <Image key={index} src={item.imageUrl} width={30} height={30} alt="Google Play" />
-                ))}
-                </div>
-            </section>
+                  <section className='section-social'>
+                      <div className="title">{socialData.title}</div>
+                      <div className='items'>
+                          {socialData.items.map((item, index) => (
+                              <Image key={index} src={item.imageUrl} width={30} height={30} alt="Google Play" />
+                          ))}
+                      </div>
+                  </section>
+              </div>
 
             <section className='section-menu'>
                 {footerData.map((menu, index) => (
-                    <div key={index}>
+                    <div className='xl:w-1/3' key={index}>
                         <div className='title' onClick={() => openMobileDropdown(index)}>
                             <div>{menu.title}</div>
                             <Image width={30} height={30} src={'/assets/icons/chevron-' + (menu.isOpen ? 'up.svg' : 'down.svg')} alt="down"/>
                         </div>
-                        {menu.isOpen && (
-                            <ul>
+                        <ul className={`${menu.isOpen || windowWidth > 1280 ? 'block' : 'hidden'}`}>
                                 {menu.items.map((item, itemIndex) => (
                                     <li key={itemIndex}>
                                         <a href={item.url}>{item.name}</a>
                                     </li>
                                 ))}
-                            </ul>
-                        )}
+                        </ul>
                     </div>
                 ))}
             </section>
