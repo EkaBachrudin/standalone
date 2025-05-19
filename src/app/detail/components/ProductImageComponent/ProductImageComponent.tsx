2585f897f81d6b10e0692531335733c89336fce0
randinovranda <vvalentine825@gmail.com>
2025-05-19 20:04:40 +0700

    Adjustment bottomsheet

---
+++ src/app/detail/components/ProductImageComponent/ProductImageComponent.tsx
@@ -0,0 +1,48 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./ProductImageComponent.scss";
+import Image from 'next/image';
+import { useState } from "react";
+import { GetDetailproductModel } from "@/domain/models/GetDetailproduct";
+
+interface  ProductImageComponentProps {
+      product: GetDetailproductModel;
+}
+const ProductImageComponent: React.FC<ProductImageComponentProps> = ({product}) => {
+      const [activeIndex, setActiveIndex] = useState(0);
+      const mobileSettings = {
+            infinite: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            centerMode: false,
+            afterChange: (current: React.SetStateAction<number>) => setActiveIndex(current)
+      };
+
+      return (
+            <>
+                  <div className="productImageComponent">
+                        <section className='mobile-image'>
+                              <Slider {...mobileSettings}>
+                                    {product?.images?.map((data, index) => (
+                                    <div className="mobile-image-item" key={index}>
+                                          <Image src={data} width={1000} height={1000} alt="filter" />
+                                    </div>
+                                    ))}
+                              </Slider>
+                              <div className="image-count">
+                                    {activeIndex+1} / {product?.images.length}
+                              </div>
+                        </section>
+
+                        <div className="dekstop-image">
+                              
+                        </div>
+                  </div>
+            </>
+      )
+}
+
+export default ProductImageComponent;
--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -2,19 +2,18 @@
 
 import React, { useEffect, useState } from 'react';
 import './detail.scss'
-import Slider from "react-slick";
+
 import Image from 'next/image';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import { GetDetailproductModel } from '@/domain/models/GetDetailproduct';
 import { digitalHubRepository } from '@/data/repositories/DigitalHubRepository';
 import DescriptionComponent from './components/DescriptionComponent/DescriptionComponent';
 import VariantComponent from './components/VariantComponent/VariantComponent';
+import ProductImageComponent from './components/ProductImageComponent/ProductImageComponent';
 
 const Detail: React.FC = () => {
 
     const [product, setProduct] = useState<GetDetailproductModel>();
-    const [activeIndex, setActiveIndex] = useState(0);
     
     useEffect(() => {
             const fetchDetail = async () => {
@@ -34,68 +33,49 @@ const Detail: React.FC = () => {
         }
     }, [product]);
 
-    const settings = {
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        centerMode: false,
-        afterChange: (current: React.SetStateAction<number>) => setActiveIndex(current)
-    };
-
     if(product) return (
         <>
             <div className="detail-container">
 
-                <section className='mobile-image'>
-                    <Slider {...settings}>
-                        {product?.images?.map((data, index) => (
-                            <div className="mobile-image-item" key={index}>
-                                <Image src={data} width={1000} height={1000} alt="filter" />
+                <div className="image-info">
+                    <ProductImageComponent product={product}></ProductImageComponent>
+
+                    <section className='product-section'>   
+                            <div className="ribbon" style={{
+                                color: product?.product_label_txtclr,
+                                backgroundColor: product?.product_label_bg
+                            }}>
+                                {product?.product_label}
                             </div>
-                        ))}
-                    </Slider>
-                    <div className="image-count">
-                        {activeIndex+1} / {product?.images.length}
-                    </div>
-                </section>
 
-                <section className='product-section'>   
-                        <div className="ribbon" style={{
-                            color: product?.product_label_txtclr,
-                            backgroundColor: product?.product_label_bg
-                        }}>
-                            {product?.product_label}
-                        </div>
-
-                        <div className="product-name">
-                            {product?.product_name}
-                        </div>
-
-                        <div className="product-price">
-                            {product?.product_price}
-                        </div>
-
-                        <div className="product-strikeout">
-                            <span className='pri'>{product?.product_strikeout_price} </span>
-                            <span className='str'>{product?.product_discount}%</span>
-                        </div>
-
-                        <div className="merchant">
-                            <div className="merchant-image">
-                                <Image src={product?.merchant_image} width={32} height={32} alt="merchantImage" />
+                            <div className="product-name">
+                                {product?.product_name}
                             </div>
-                            <div className="merchant-info">
-                                <div className="merchant-name">
-                                    {product?.merchant_name}
+
+                            <div className="product-price">
+                                {product?.product_price}
+                            </div>
+
+                            <div className="product-strikeout">
+                                <span className='pri'>{product?.product_strikeout_price} </span>
+                                <span className='str'>{product?.product_discount}%</span>
+                            </div>
+
+                            <div className="merchant">
+                                <div className="merchant-image">
+                                    <Image src={product?.merchant_image} width={32} height={32} alt="merchantImage" />
                                 </div>
-                                <div className="merchant-location">
-                                    {product?.merchant_location}
+                                <div className="merchant-info">
+                                    <div className="merchant-name">
+                                        {product?.merchant_name}
+                                    </div>
+                                    <div className="merchant-location">
+                                        {product?.merchant_location}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                </section>
+                    </section>
+                </div>
 
                 <VariantComponent 
                     variant_group={product?.variant_group} 
